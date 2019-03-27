018f7e97801a81d296c2b32c9c58fef9da2d2c21
15reichar <5reichar@informatik.uni-hamburg.de>
2019-03-27 17:25:16 +0100

    - Node zum visualisieren der Spline hinzugefügt     - erstmal provisorische beispiel implementation     - aufbau angepasst     - nutzt noch keine splines

---
+++ include/Utils/VisualSplinesNode.hpp
@@ -0,0 +1,22 @@
+#include <stdint.h>
+#include <visualization_msg/Marker.h>
+#include <Spline.hpp>
+
+class VisualSplinesNode
+{
+public:
+    VisualSplinesNode(int argc, char** argv);
+    ~VisualSplinesNode();
+
+    visualization_msgs::Marker get_marker(string marker_namespace, uint8_t marker_id);
+    void set_marker_properties(visualization_msgs::Marker & marker);
+    void publish_marker(visualization_msgs::Marker & marker);
+
+private:
+    uint32_t get_shape();
+    void set_marker_scale();
+    void set_marker_color();
+
+    uint32_t m_uint_shape;
+    ros::Publisher m_ros_marker_publisher;
+};
