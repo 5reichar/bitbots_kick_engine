a5eb45df40b0ba3b4273030df65f1ff216a6d2e2
15reichar <5reichar@informatik.uni-hamburg.de>
2019-03-28 14:27:45 +0100

    - formatting

--- src/Utils/VisualSplinesNode.cpp
+++ src/Utils/VisualSplinesNode.cpp
@@ -6,7 +6,8 @@ int main(int argc, char** argv)
     VisualSplinesService vs_service = new VisualSplinesService(argc, argv);
     ros::Rate rate(1);
 
-    while(ros::ok()){
+    while(ros::ok())
+    {
         auto marker = vs_service->get_marker("spline_shapes", 0);
         vs_service->set_marker_properties(marker)
         vs_service->publish_marker(marker);
