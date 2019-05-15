48545084b79cb7456984cca75f41a471f279b676
15reichar <5reichar@informatik.uni-hamburg.de>
2019-05-15 12:33:54 +0200

    Copied TrajectoryUtils, from meta, as TrajectoryService. Rebuild as Class with static functions and fitted for the Project

---
+++ include/Utils/TrajectoryService.hpp
@@ -0,0 +1,78 @@
+/*
+This code is largely based on the original code by Quentin "Leph" Rouxel and Team Rhoban.
+The original files can be found at:
+https://github.com/Rhoban/model/
+*/
+#ifndef TRAJECTORYSERVICE_HPP
+#define TRAJECTORYSERVICE_HPP
+
+#include "bitbots_splines/Curve.hpp"
+#include "SplineContainer.hpp"
+#include <Eigen/Dense>
+
+namespace bitbots_splines
+{
+
+class TrajectoryService
+{
+    /**
+     * Simple typedef for trajectories container
+     */
+    typedef SplineContainer<Curve> Trajectories;
+
+    /**
+     * Return initialized trajectories for
+     * trunk/foot ik cartesian with empty splines
+     */
+    static Trajectories TrajectoriesInit();
+
+    /**
+     * Compute from given spline container
+     * trajectory Cartesian trunk and foot
+     * position/velocity/acceleration
+     * and assign it to given vector
+     */
+    static void TrajectoriesTrunkFootPos(
+        double t, const Trajectories &traj,
+        Eigen::Vector3d &trunkPos,
+        Eigen::Vector3d &trunkAxis,
+        Eigen::Vector3d &footPos,
+        Eigen::Vector3d &footAxis);
+
+    static void TrajectoriesTrunkFootVel(
+        double t, const Trajectories &traj,
+        Eigen::Vector3d &trunkPosVel,
+        Eigen::Vector3d &trunkAxisVel,
+        Eigen::Vector3d &footPosVel,
+        Eigen::Vector3d &footAxisVel);
+
+    static void TrajectoriesTrunkFootAcc(
+        double t, const Trajectories &traj,
+        Eigen::Vector3d &trunkPosAcc,
+        Eigen::Vector3d &trunkAxisAcc,
+        Eigen::Vector3d &footPosAcc,
+        Eigen::Vector3d &footAxisAcc);
+
+    static void TrajectoriesSupportFootState(
+        double t, const Trajectories &traj,
+        bool &isDoubleSupport,
+        bool &isLeftsupportFoot);
+
+    /**
+     * Default Cartesian state check function.
+     * Return positive cost value
+     * if given time and Cartesian state are outside
+     * standard valid range
+     */
+    static double DefaultCheckState(
+        const Eigen::VectorXd &params,
+        double t,
+        const Eigen::Vector3d &trunkPos,
+        const Eigen::Vector3d &trunkAxis,
+        const Eigen::Vector3d &footPos,
+        const Eigen::Vector3d &footAxis);
+};
+
+} // namespace bitbots_splines
+
+#endif
