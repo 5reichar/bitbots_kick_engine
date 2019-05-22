afb69be5fd5fc1a2c610ad51d3f3aacb5fab075c
15reichar <5reichar@informatik.uni-hamburg.de>
2019-05-22 15:17:28 +0200

    KickEngineNode Revamp

--- bitbots_kick_engine/include/KickEngineNode.hpp
+++ bitbots_kick_engine/include/KickEngineNode.hpp
@@ -1,13 +1,25 @@
 #ifndef KICKENGINENODE_HPP
 #define KICKENGINENODE_HPP
 
+#include <humanoid_league_msgs/RobotControlState.h>
+
 class KickEngineNode
 {
-private:
-    /* data */
 public:
     KickEngineNode(/* args */);
     ~KickEngineNode();
+
+    void kick();
+
+private:
+    void initialise_ros_subcribtions();
+    void kick_ball(geometry_msgs::Vector3 const &const ball_position, geometry_msgs::Vector3 const &const target_position);
+    void publish_kick();
+
+    void robot_state_callback(const humanoid_league_msgs::RobotControlState msg);
+    void kick_callback();
+
+    humanoid_league_msgs::RobotControlState::state m_robot_state;
 };
 
 #endif
--- bitbots_kick_engine/src/KickEngineNode.cpp
+++ bitbots_kick_engine/src/KickEngineNode.cpp
@@ -1,14 +1,21 @@
 #include <ros/ros.h>
 #include <geometry_msgs/Vector3.h>
-#include "bitbots_kick_engine/KickEngineService.hpp"
 
-void publish_kick(KickEngineService *kick_engine_service)
+void KickEngineNode::kick()
 {
-    //TODO implement publishing of kick
+    // TODO implementation
 }
 
-void kick_ball(geometry_msgs::Vector3 const &const ball_position, geometry_msgs::Vector3 const &const target_position)
+void KickEngineNode::initialise_ros_subcribtions()
 {
+    // TODO implementation
+}
+
+void KickEngineNode::kick_ball(geometry_msgs::Vector3 const &const ball_position, geometry_msgs::Vector3 const &const target_position)
+{
+    // TODO implementation
+
+    /*
     KickEngineService kick_engine_service;
 
     kick_engine_service.set_ball(ball_position.x, ball_position.y, ball_position.z);
@@ -16,6 +23,22 @@ void kick_ball(geometry_msgs::Vector3 const &const ball_position, geometry_msgs:
     kick_engine_service.calc_kick();
 
     publish_kick(&kick_engine_service);
+    */
+}
+
+void KickEngineNode::publish_kick()
+{
+    // TODO implementation
+}
+
+void KickEngineNode::robot_state_callback(const humanoid_league_msgs::RobotControlState msg)
+{
+    // TODO implementation
+}
+
+void KickEngineNode::kick_callback()
+{
+    // TODO implementation
 }
 
 int main(int argc, char **argv)
