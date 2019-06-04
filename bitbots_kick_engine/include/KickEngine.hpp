7f7c0a6c1b63e02878643212b7552cb1abbc087d
W. Adrian Reichardt <5reichar@informatik.uni-hamburg.de>
2019-06-04 10:19:46 +0200

    First Draft for KickEngine Added

---
+++ bitbots_kick_engine/include/KickEngine.hpp
@@ -0,0 +1,30 @@
+#ifndef KICKENGINE_HPP
+#define KICKENGINE_HPP
+
+#include <humanoid_league_msgs/RobotControlState.h>
+
+class KickEngine
+{
+    public:
+        enum class KickType : uint16_t
+        {
+            standard = 0
+        };
+
+    KickEngine(const humanoid_league_msgs::RobotControlState msg);
+
+    void set_param();
+    void set_robot_state(const humanoid_league_msgs::RobotControlState msg);
+    void kick(geometry_msgs::Vector3 const &const ball_position, geometry_msgs::Vector3 const &const target_position);
+
+    private:
+
+    geometry_msgs::Vector3 get_kick_start_position();
+    void move_left_feet_to_position(geometry_msgs::Vector3 position);
+    void move_right_feet_to_position(geometry_msgs::Vector3 position);
+    void move_feet_to_position(geometry_msgs::Vector3 position);
+
+    humanoid_league_msgs::RobotControlState::state m_robot_state;
+}
+
+#endif
