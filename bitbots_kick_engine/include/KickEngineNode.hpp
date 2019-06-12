8f12d787f5425c6866eaeb4a9902da9b8a24c328
15reichar <5reichar@informatik.uni-hamburg.de>
2019-06-12 14:20:01 +0200

    Added init stuff(Subs, Pubs, Movit and bioIK) based on the init from QuinticWalkingNode

--- bitbots_kick_engine/include/KickEngineNode.hpp
+++ bitbots_kick_engine/include/KickEngineNode.hpp
@@ -2,6 +2,7 @@
 #define KICKENGINENODE_HPP
 
 #include "KickEngine.hpp"
+#include "ros/ros.h"
 
 class KickEngineNode
 {
@@ -24,6 +25,23 @@ private:
 
     KickEngine m_kick_enginge;
     uint16_t m_uint_odometry_publish_factor;
+
+    ros::NodeHandle m_ros_node_handle;
+
+    ros::Publisher m_ros_publisher_controller_command;
+    ros::Publisher m_ros_publisher_odometry;
+    ros::Publisher m_ros_publisher_support;
+    ros::Publisher m_ros_publisher_debug;
+    ros::Publisher m_ros_publisher_debug_marker;
+
+    ros::Subscriber m_ros_subsciber_kick;
+    ros::Subscriber m_ros_subsciber_robot_state;
+
+    robot_model::RobotModelPtr m_kinematic_model;
+    robot_state::RobotStatePtr m_goal_state;
+    robot_state::RobotStatePtr m_current_state;
+
+    bitbots_ik::BioIKSolver m_bio_ik_solver;
 };
 
 #endif
--- bitbots_kick_engine/src/KickEngineNode.cpp
+++ bitbots_kick_engine/src/KickEngineNode.cpp
@@ -1,5 +1,38 @@
-#include <ros/ros.h>
+#include "KickEngineNode.hpp"
 #include <geometry_msgs/Vector3.h>
+
+KickEngineNode::KickEngineNode()
+{
+    initialise_ros_subcribtions();
+    initialise_ros_publisher();
+
+    robot_model_loader::RobotModelLoader robot_model_loader("/robot_description", false);
+    robot_model_loader.loadKinematicsSolvers(
+        kinematics_plugin_loader::KinematicsPluginLoaderPtr(
+            new kinematics_plugin_loader::KinematicsPluginLoader()));
+
+    m_kinematic_model = robot_model_loader.getModel();
+
+    m_goal_state.reset(new robot_state::RobotState(_kinematic_model));
+    m_goal_state->setToDefaultValues();
+    // we have to set some good initial position in the goal state, since we are using a gradient
+    // based method. Otherwise, the first step will be not correct
+    std::vector<std::string> names_vec = {"LHipPitch", "LKnee", "LAnklePitch", "RHipPitch", "RKnee", "RAnklePitch"};
+    std::vector<double> pos_vec = {0.7, -1.0, -0.4, -0.7, 1.0, 0.4};
+    for (int i = 0; i < names_vec.size(); i++)
+    {
+        // besides its name, this method only changes a single joint position...
+        m_goal_state->setJointPositions(names_vec[i], &pos_vec[i]);
+    }
+
+    m_current_state.reset(new robot_state::RobotState(_kinematic_model));
+    m_current_state->setToDefaultValues();
+
+    m_bio_ik_solver = bitbots_ik::BioIKSolver(*m_kinematic_model->getJointModelGroup("All"),
+                                              *m_kinematic_model->getJointModelGroup("LeftLeg"),
+                                              *m_kinematic_model->getJointModelGroup("RightLeg"));
+    m_bio_ik_solver.set_use_approximate(true);
+}
 
 void KickEngineNode::kick()
 {
@@ -9,11 +42,19 @@ void KickEngineNode::kick()
 void KickEngineNode::initialise_ros_subcribtions()
 {
     // TODO implementation
+    m_ros_subsciber_kick = m_ros_node_handle.subscribe("kick", 1, &KickEngineNode::kick_callback, this, ros::TransportHints().tcpNoDelay());
+    m_ros_subsciber_robot_state = m_ros_node_handle.subscribe("robot_state", 1, &KickEngineNode::robot_state_callback, this, ros::TransportHints().tcpNoDelay());
 }
 
 void KickEngineNode::initialise_ros_publisher()
 {
     // TODO implementation
+    m_ros_publisher_controller_command = m_ros_node_handle.advertise<bitbots_msgs::JointCommand>("kick_motor_goals", 1);
+    m_ros_publisher_odometry = m_ros_node_handle.advertise<nav_msgs::Odometry>("kick_odometry", 1);
+    m_ros_publisher_support = m_ros_node_handle.advertise<std_msgs::Char>("kick_support_state", 1);
+
+    m_ros_publisher_debug = m_ros_node_handle.advertise<bitbots_quintic_walk::WalkingDebug>("kick_debug", 1);
+    m_ros_publisher_debug_marker = m_ros_node_handle.advertise<visualization_msgs::Marker>("kick_debug_marker", 1);
 }
 
 void KickEngineNode::kick_ball(geometry_msgs::Vector3 const &const ball_position, geometry_msgs::Vector3 const &const target_position)
