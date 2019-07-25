2cd529509ff7f19c4b3079b0b690c0e789d5abd4
15reichar <5reichar@informatik.uni-hamburg.de>
2019-07-25 15:05:30 +0200

    partial implementation of KickFactory

--- bitbots_kick_engine/include/engine/KickFactory.hpp
+++ bitbots_kick_engine/include/engine/KickFactory.hpp
@@ -9,11 +9,24 @@ public:
 	virtual bitbots_splines::SplineContainer make_kick_trajection(struct3d & const ball, struct3d & const goal);
 
 private:
-	struct3d calculate_kick_start(struct3d & const ball, struct3d& const goal);
-	struct3d get_foot_position(bool const kick_with_left);
-	bool check_kicking_with_left(struct3d & const kick_start, struct3d & const ball);
+	void init(struct3d& const ball, struct3d& const goal);
+	void reset();
 
-	virtual bitbots_splines::SplineContainer make_kick_trajection(struct3d& const foot_current, struct3d& const foot_kick_start, struct3d& const ball_current, struct3d& const ball_goal, bool& const kick_with_left);
+	double calculate_angle(double const x, double const y);
+	struct3d calculate_kick_start();
+	struct3d get_foot_position();
+	bool check_kicking_with_left();
+
+	virtual bitbots_splines::SplineContainer make_kick_trajection();
+
+	struct3d const* m_pc_current_foot_position;
+	struct3d const* m_pc_kick_start_foot_position;
+	struct3d const* m_pc_current_ball_position;
+	struct3d const* m_pc_ball_goal_position;
+	bool m_b_kick_with_left;
+
+	double m_d_angle_ball_goal;
+	double m_d_angle_robot_ball;
 };
 
 #endif
--- bitbots_kick_engine/src/engine/KickFactory.cpp
+++ bitbots_kick_engine/src/engine/KickFactory.cpp
@@ -2,29 +2,130 @@
 
 bitbots_splines::SplineContainer KickFactory::make_kick_trajection(struct3d & const ball, struct3d & const goal)
 {
-	auto kick_start = calculate_kick_start(ball, goal);
-	auto kick_with_left_foot = check_kicking_with_left(kick_start, ball);
-	auto kick_foot = get_foot_position(kick_with_left_foot);
+	init(ball, goal);
 
-	return make_kick_trajection(kick_foot, kick_start, ball, goal, kick_with_left_foot);
+	auto kick_trajectory = make_kick_trajection();
+
+	reset();
+
+	return kick_trajectory;
 }
 
-struct3d KickFactory::calculate_kick_start(struct3d & const ball, struct3d & const goal)
+void KickFactory::init(struct3d& const ball, struct3d& const goal)
+{
+	m_d_angle_robot_ball = calculate_angle(ball.x, ball.y);
+	m_d_angle_ball_goal = calculate_angle(goal.x - ball.x, goal.y - ball.y);
+
+	m_pc_current_foot_position = get_foot_position();
+	m_pc_kick_start_foot_position = calculate_kick_start();
+	m_pc_current_ball_position = ball;
+	m_pc_ball_goal_position = goal;
+	m_b_kick_with_left = check_kicking_with_left();
+}
+
+void KickFactory::reset()
+{
+	delete m_pc_current_foot_position;
+	delete m_pc_kick_start_foot_position;
+	delete m_pc_current_ball_position;
+	delete m_pc_ball_goal_position;
+}
+
+double KickFactory::calculate_angle(double const x, double const y)
+{
+	double angle = y == 0.0 ? std::atan(std::abs(x) / std::abs(y));
+
+	if (x < 0 && y > 0)
+	{
+		angle += 270.0;
+	}
+	else if (x < 0 && y < 0)
+	{
+		angle += 180.0;
+	}
+	else if (x > 0 && y < 0)
+	{
+		angle += 90.0;
+	}
+
+	return angle;
+}
+
+struct3d KickFactory::calculate_kick_start()
+{
+	// TODO: make dynamic (adjustable via KickEngineParameter and/or Kick)
+	struct3d kick_start_position;
+
+	if (m_d_angle_robot_ball > 315 || m_d_angle_robot_ball < 45)
+	{
+		// TODO: Replace placeholder-values
+		kick_start_position.x = 0;
+		kick_start_position.y = -10;
+		kick_start_position.z = -10;
+	}
+	else if (m_d_angle_robot_ball > 135 || m_d_angle_robot_ball < 225)
+	{
+		// TODO: Replace placeholder-values
+		kick_start_position.x = 0;
+		kick_start_position.y = 10;
+		kick_start_position.z = 5;
+	}
+	else if (m_d_angle_robot_ball == 90 || m_d_angle_robot_ball == 270)
+	{
+		// TODO: Replace placeholder-values
+		kick_start_position.x = 0;
+		kick_start_position.y = 0;
+		kick_start_position.z = 0;
+	}
+	else if (m_d_angle_robot_ball > 45 || m_d_angle_robot_ball < 90)
+	{
+		// TODO: Replace placeholder-values
+		kick_start_position.x = -5;
+		kick_start_position.y = 5;
+		kick_start_position.z = 0;
+	}
+	else if (m_d_angle_robot_ball > 90 || m_d_angle_robot_ball < 135)
+	{
+		// TODO: Replace placeholder-values
+		kick_start_position.x = -5;
+		kick_start_position.y = -5;
+		kick_start_position.z = 0;
+	}
+	else if (m_d_angle_robot_ball > 225 || m_d_angle_robot_ball < 270)
+	{
+		// TODO: Replace placeholder-values
+		kick_start_position.x = 5;
+		kick_start_position.y = -5;
+		kick_start_position.z = 0;
+	}
+	else if (m_d_angle_robot_ball > 270 || m_d_angle_robot_ball < 315)
+	{
+		// TODO: Replace placeholder-values
+		kick_start_position.x = 5;
+		kick_start_position.y = 5;
+		kick_start_position.z = 0;
+	}
+
+	return kick_start_position;
+}
+
+struct3d KickFactory::get_foot_position()
 {
 	return struct3d();
 }
 
-struct3d KickFactory::get_foot_position(bool const kick_with_left)
+bool KickFactory::check_kicking_with_left()
 {
-	return struct3d();
+	// TODO: make dynamic (adjustable via KickEngineParameter and/or Kick)
+	return m_d_angle_robot_ball > 180;
 }
 
-bool KickFactory::check_kicking_with_left(struct3d & const kick_start, struct3d & const ball)
+bitbots_splines::SplineContainer KickFactory::make_kick_trajection()
 {
-	return false;
-}
+	// TODO: implementation
+	bitbots_splines::SplineContainer kick_trajection;
 
-bitbots_splines::SplineContainer KickFactory::make_kick_trajection(struct3d & const foot_current, struct3d & const foot_kick_start, struct3d & const ball_current, struct3d & const ball_goal, bool const kick_with_left)
-{
-	return bitbots_splines::SplineContainer();
+
+
+	return kick_trajection;
 }
