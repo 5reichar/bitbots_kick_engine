22cbe1a673750f9d8c410fde55207d472bb3648a
5reichar <5reichar@informatik.uni-hamburg.de>
2020-05-14 15:51:53 +0200

    Set Bases Values for ThrowRespone small fixes

--- bitbots_throw_engine/include/utility/throw_utilities.h
+++ bitbots_throw_engine/include/utility/throw_utilities.h
@@ -3,38 +3,41 @@
 
 #include <string>
 #include "parameter/struct3d.h"
-#include <geometry_msgs/Quaternion.h>
+// #include<tf2/Transform.h>
+//TODO: Replace include
+#include "../../unchanged/tf2/include/tf2/LinearMath/Transform.h"
 
 enum class CurvePurpose
 {
-    trunk,
-    left_foot,
-    right_foot,
-    left_hand,
-    right_hand,
-    arms,
-    left_elbow,
-    left_shoulder_pitch,
-    left_shoulder_roll,
-    right_elbow,
-    right_shoulder_pitch,
-    right_shoulder_roll
+  trunk,
+  left_foot,
+  right_foot,
+  left_hand,
+  right_hand,
+  arms,
+  left_elbow,
+  left_shoulder_pitch,
+  left_shoulder_roll,
+  right_elbow,
+  right_shoulder_pitch,
+  right_shoulder_roll
 };
 
 struct ThrowRequest
 {
-    Struct3d ball_position_;
-    Struct3d goal_position_;
+  Struct3d ball_position_;
+  Struct3d goal_position_;
 };
 
 struct ThrowResponse
 {
-  tf2::Transform support_foot_to_flying_foot;
-  tf2::Transform support_foot_to_trunk;
+  tf2::Transform support_foot_to_left_hand_;
+  tf2::Transform support_foot_to_right_hand_;
+  tf2::Transform support_foot_to_trunk_;
 
   // additional information for visualization
-  double phase;
-  double traj_time;
+  double phase_;
+  double traj_time_;
 };
 
 static std::string get_joint_name(CurvePurpose joint)
@@ -43,25 +46,25 @@ static std::string get_joint_name(CurvePurpose joint)
 
   switch (joint)
   {
-    case arms:
+    case CurvePurpose::arms:
       joint_name = "Arms";
       break;
-    case left_elbow:
+    case CurvePurpose::left_elbow:
       joint_name = "LElbow";
       break;
-    case left_shoulder_pitch:
+    case CurvePurpose::left_shoulder_pitch:
       joint_name = "LShoulderPitch";
       break;
-    case left_shoulder_roll:
+    case CurvePurpose::left_shoulder_roll:
       joint_name = "LShoulderRoll";
       break;
-    case right_elbow:
+    case CurvePurpose::right_elbow:
       joint_name = "RElbow";
       break;
-    case right_shoulder_pitch:
+    case CurvePurpose::right_shoulder_pitch:
       joint_name = "RShoulderPitch";
       break;
-    case right_shoulder_roll:
+    case CurvePurpose::right_shoulder_roll:
       joint_name = "RShoulderRoll";
       break;
     default:
