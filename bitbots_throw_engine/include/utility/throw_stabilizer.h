dd3fe1d3e2b5d8f789acffe729bb36f47b52a192
5reichar <5reichar@informatik.uni-hamburg.de>
2020-05-14 15:56:31 +0200

    first draft for throw_stabilizer implementation don't work, don't really know why

--- bitbots_throw_engine/include/utility/throw_stabilizer.h
+++ bitbots_throw_engine/include/utility/throw_stabilizer.h
@@ -1,15 +1,16 @@
 #ifndef THROW_STABILIZER_H
 #define THROW_STABILIZER_H
 
-#include "throws/throw_curves/throw_curve.h"
+#include <memory>
+#include "utility/throw_utilities.h"
 #include "../../bitbots_spline/include/utils/abstract_stabilizer.h"
 
-class ThrowStabilizer : public bitbots_splines::AbstractStabilizer<std::shared_ptr<ThrowCurve>>
+class ThrowStabilizer : public bitbots_splines::AbstractStabilizer<ThrowResponse>
 {
 public:
-  WalkStabilizer();
+  ThrowStabilizer();
   virtual void reset() override;
-  virtual std::unique_ptr<bio_ik::BioIKKinematicsQueryOptions> stabilize(const std::shared_ptr<ThrowCurve> &positions) override;
+  virtual std::unique_ptr<bio_ik::BioIKKinematicsQueryOptions> stabilize(const ThrowResponse & response) override;
 
 private:
 };
--- bitbots_throw_engine/src/utility/throw_stabilizer.cpp
+++ bitbots_throw_engine/src/utility/throw_stabilizer.cpp
@@ -1,4 +1,5 @@
 #include "utility/throw_stabilizer.h"
+//#include "../../bitbots_spline/include/utils/reference_goals.h"
 
 ThrowStabilizer::ThrowStabilizer()
 {
@@ -9,11 +10,41 @@ void ThrowStabilizer::reset()
 {
 }
 
-std::unique_ptr<bio_ik::BioIKKinematicsQueryOptions> ThrowStabilizer::stabilize(const std::shared_ptr<ThrowCurve> &positions)
+std::unique_ptr<bio_ik::BioIKKinematicsQueryOptions> ThrowStabilizer::stabilize(const ThrowResponse & response)
 {
-  auto ik_options = std::make_unique<bio_ik::BioIKKinematicsQueryOptions>();
+  std::unique_ptr<bio_ik::BioIKKinematicsQueryOptions> ik_options;
   ik_options->replace = true;
   ik_options->return_approximate_solution = true;
+
+  /*
+  //TODO: Fixing this; ReferencePoseGoal creates errors in compiler, don't know why
+  // trunk goal
+  auto *trunk_goal = new ReferencePoseGoal();
+  trunk_goal->setPosition(response.support_foot_to_trunk_.getOrigin());
+  trunk_goal->setOrientation(response.support_foot_to_trunk_.getRotation());
+  trunk_goal->setLinkName("base_link");
+  trunk_goal->setReferenceLinkName("l_sole"); //TODO: rework maybe
+  trunk_goal->setWeight(1);
+  ik_options->goals.emplace_back(trunk_goal);
+
+  // left hand goal
+  auto *left_hand_goal = new ReferencePoseGoal();
+  left_hand_goal->setPosition(response.support_foot_to_left_hand_.getOrigin());
+  left_hand_goal->setOrientation(response.support_foot_to_left_hand_.getRotation());
+  left_hand_goal->setLinkName("base_link");
+  left_hand_goal->setReferenceLinkName("l_sole"); //TODO: rework maybe
+  left_hand_goal->setWeight(1);
+  ik_options->goals.emplace_back(left_hand_goal);
+
+  // right hand goal
+  auto *right_hand_goal = new ReferencePoseGoal();
+  right_hand_goal->setPosition(response.support_foot_to_right_hand_.getOrigin());
+  right_hand_goal->setOrientation(response.support_foot_to_right_hand_.getRotation());
+  right_hand_goal->setLinkName("base_link");
+  right_hand_goal->setReferenceLinkName("r_sole"); //TODO: rework maybe
+  right_hand_goal->setWeight(1);
+  ik_options->goals.emplace_back(right_hand_goal);
+  */
 
   return std::move(ik_options);
 }
