917d6e97b083b423c571b95dbce7a1b344bf1004
5reichar <5reichar@informatik.uni-hamburg.de>
2020-05-20 15:28:16 +0200

    part 2

--- bitbots_throw_engine/src/ros_interface/publisher/debug_publisher.cpp
+++ bitbots_throw_engine/src/ros_interface/publisher/debug_publisher.cpp
@@ -42,9 +42,9 @@ void DebugPublisher::publish_ik_debug(ThrowResponse response, bitbots_splines::J
     tf2::toMsg(response.support_foot_to_right_hand_, msg.right_hand_goal);
     tf2::toMsg(response.support_foot_to_trunk_, msg.trunk_goal);
 
-    publishArrowMarker("throw_engine_left_hand_goal", "base_link", msg.left_hand_goal, 0, 1, 0, 1);
-    publishArrowMarker("throw_engine_right_hand_goal", "base_link", msg.right_hand_goal, 1, 0, 0, 1);
-    publishArrowMarker("throw_engine_trunk_goal", "base_link", msg.trunk_goal, 0, 0, 1, 1);
+    publish_arrow_marker("throw_engine_left_hand_goal", "base_link", msg.left_hand_goal, 0, 1, 0, 1);
+    publish_arrow_marker("throw_engine_right_hand_goal", "base_link", msg.right_hand_goal, 1, 0, 0, 1);
+    publish_arrow_marker("throw_engine_trunk_goal", "base_link", msg.trunk_goal, 0, 0, 1, 1);
 
     ros_publisher_debug_.publish(msg);
 }
