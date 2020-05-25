4e9d0f4bdc54dffaf44590b16d988f85c97f494c
5reichar <5reichar@informatik.uni-hamburg.de>
2020-05-25 17:40:50 +0200

    added aditional functions to send console messages in the ros system

--- bitbots_throw_engine/include/ros_interface/publisher/system_publisher.h
+++ bitbots_throw_engine/include/ros_interface/publisher/system_publisher.h
@@ -8,15 +8,36 @@ class SystemPublisher
 {
     //TODO: cleanup
 public:
+    static void publish_fatal(const std::string & messsage)
+    {
+        ROS_FATAL_STREAM(messsage);
+    };
+
+    static void publish_error(const std::string & messsage)
+    {
+        ROS_ERROR_STREAM(messsage);
+    };
+
     static void publish_warning(const std::string & messsage)
     {
         ROS_WARN_STREAM(messsage);
+    };
+
+    static void publish_info(const std::string & messsage)
+    {
+        ROS_INFO_STREAM(messsage);
+    };
+
+    static void publish_debug(const std::string & messsage)
+    {
+        ROS_DEBUG_STREAM(messsage);
     };
 
     static void publish_runtime_error(const std::runtime_error& error)
     {
         ROS_ERROR_STREAM(error.what());
     };
+
 
 private:
 };
