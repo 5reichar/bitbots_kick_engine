fdddd5dcb7fd2b992fb1463ac7719afd20896848
5reichar <5reichar@informatik.uni-hamburg.de>
2020-05-29 12:32:18 +0200

    bio ik and dynamic reconfiguration rework part 1

--- bitbots_throw_engine/include/ros_interface/throw_node.h
+++ bitbots_throw_engine/include/ros_interface/throw_node.h
@@ -2,13 +2,14 @@
 #define THROW_NODE_H
 
 #include "ros/ros.h"
-#include "engine/throw_engine.h"
-#include <bitbots_throw_engine/throw_action.h>
-#include "ros_interface/publisher/ros_publisher_facade.h"
-#include "parameter/throw_engine_parameter_builder.h"
-#include "parameter/throw_node_parameter_builder.h"
-#include "parameter/throw_type_parameter_builder.h"
 #include "utility/throw_ik.h"
+#include "engine/throw_engine.h"
+#include "parameter/throw_node_parameter_builder.h"
+
+#include <dynamic_reconfigure/server.h>
+#include <bitbots_throw_engine/throw_action.h>
+#include <bitbots_throw_engine/throw_paramsConfig.h>
+#include <bitbots_throw_engine/throw_engine_paramsConfig.h>
 
 class ThrowNode
 {
@@ -21,21 +22,21 @@ private:
     void init_dynamic_reconfiguration();
 
 	void throw_callback(const bitbots_throw_engine::throw_action action);
-
-    void throw_engine_params_config_callback(bitbots_throw_engine::throw_engine_paramsConfig & config , uint32_t level);
     void throw_params_config_callback(bitbots_throw_engine::throw_paramsConfig & config , uint32_t level);
+    void throw_engine_params_config_callback(bitbots_throw_engine::throw_engine_paramsConfig & config , uint32_t level);
 
     ThrowRequest create_throw_request(const bitbots_throw_engine::throw_action action);
 
+    ThrowIK ik_;
     std::unique_ptr<ThrowEngine> up_throw_engine_;
     std::shared_ptr<ThrowNodeParameter> sp_node_parameter_;
 
     ros::NodeHandle ros_node_handle_;
-    std::unique_ptr<RosPublisherFacade> up_publisher_facade_;
-    std::unique_ptr<ThrowIK> up_throw_ik_;
-
     ros::Subscriber ros_subsciber_throw_;
     ros::Subscriber ros_subsciber_robot_state_;
+
+    dynamic_reconfigure::Server<bitbots_throw_engine::throw_paramsConfig> throw_param_server_;
+    dynamic_reconfigure::Server<bitbots_throw_engine::throw_engine_paramsConfig> engine_param_server_;
 };
 
 #endif
--- bitbots_throw_engine/src/ros_interface/throw_node.cpp
+++ bitbots_throw_engine/src/ros_interface/throw_node.cpp
@@ -1,33 +1,40 @@
 #include "ros_interface/throw_node.h"
 
-#include <dynamic_reconfigure/server.h>
 #include <moveit/kinematics_base/kinematics_base.h>
 #include <moveit/robot_model_loader/robot_model_loader.h>
 
 #include "utility/throw_utilities.h"
 #include "utility/throw_stabilizer.h"
 
+#include "ros_interface/publisher/system_publisher.h"
+#include "ros_interface/publisher/ros_publisher_facade.h"
+
+#include "parameter/throw_engine_parameter_builder.h"
+#include "parameter/throw_type_parameter_builder.h"
+
 ThrowNode::ThrowNode()
 {
 	//TODO: testing
 	//TODO: cleanup
-
+	sp_node_parameter_ = ThrowNodeParameterBuilder::build_default();
 	ros_node_handle_.param<bool>("/simulation_active", sp_node_parameter_->simulation_active_, false);
-
-	up_publisher_facade_.reset(new RosPublisherFacade(ros_node_handle_, sp_node_parameter_));
 	init_ros_subcribtions();
 	init_dynamic_reconfiguration();
+	SystemPublisher::publish_warning("Init dynamic reconfiguration done");
 
 	//load MoveIt! model
 	robot_model_loader::RobotModelLoader robot_model_loader("/robot_description", false);
+
 	robot_model_loader.loadKinematicsSolvers(std::make_shared<kinematics_plugin_loader::KinematicsPluginLoader>());
-	 robot_model::RobotModelPtr kinematic_model(robot_model_loader.getModel());
+	moveit::core::RobotModelPtr kinematic_model;
+	kinematic_model = robot_model_loader.getModel();
 	if (!kinematic_model)
 	{
-		ROS_FATAL("No robot model loaded, killing quintic walk.");
+		SystemPublisher::publish_fatal("No robot model loaded, killing throw engine.");
 		exit(1);
 	}
-	up_throw_ik_->init(kinematic_model);
+
+	ik_.init(kinematic_model);
 }
 
 void ThrowNode::init_ros_subcribtions()
@@ -42,16 +49,15 @@ void ThrowNode::init_dynamic_reconfiguration()
 {
 	//TODO: testing
 	//TODO: cleanup
+	SystemPublisher::publish_info("start init_dynamic_reconfiguration");
 
-	dynamic_reconfigure::Server<bitbots_throw_engine::throw_engine_paramsConfig> engine_param_server;
-	dynamic_reconfigure::Server<bitbots_throw_engine::throw_engine_paramsConfig>::CallbackType engine_params;
-	engine_params = boost::bind(&ThrowNode::throw_engine_params_config_callback, this, _1, _2);
-	engine_param_server.setCallback(engine_params);
+	engine_param_server_.setCallback(boost::bind(&ThrowNode::throw_engine_params_config_callback, this, _1, _2));
 
-	dynamic_reconfigure::Server<bitbots_throw_engine::throw_paramsConfig> throw_param_server;
-	dynamic_reconfigure::Server<bitbots_throw_engine::throw_paramsConfig>::CallbackType throw_params;
-	throw_params = boost::bind(&ThrowNode::throw_params_config_callback, this, _1, _2);
-	throw_param_server.setCallback(throw_params);
+	SystemPublisher::publish_fatal("throw engine parameter done");
+
+	throw_param_server_.setCallback(boost::bind(&ThrowNode::throw_params_config_callback, this, _1, _2));
+
+	SystemPublisher::publish_warning("throw parameter done");
 }
 
 void ThrowNode::throw_callback(const bitbots_throw_engine::throw_action action)
@@ -61,7 +67,9 @@ void ThrowNode::throw_callback(const bitbots_throw_engine::throw_action action)
 
 	ThrowStabilizer stabilizer;
 	ros::Rate loopRate(sp_node_parameter_->engine_frequency_);
-	up_publisher_facade_->prepare_publisher_for_throw();
+	RosPublisherFacade publisher_facade(ros_node_handle_, sp_node_parameter_);
+
+	publisher_facade.prepare_publisher_for_throw();
 	up_throw_engine_->set_goals(create_throw_request(action));
 
 	while (ros::ok())
@@ -72,18 +80,19 @@ void ThrowNode::throw_callback(const bitbots_throw_engine::throw_action action)
 
 		try
 		{
-			joint_goals = up_throw_ik_->calculate(std::move(ik_goals));
+			joint_goals = ik_.calculate(std::move(ik_goals));
 		}
 		catch(const std::runtime_error& e)
 		{
-			std::cerr << e.what() << '\n';
+			SystemPublisher::publish_runtime_error(e);
+
 			// maybe add some more diagnostic logic
 			joint_goals = bitbots_splines::JointGoals();
 		}
 		
-		up_publisher_facade_->publish_throw(joint_goals);
-		up_publisher_facade_->publish_odometry();
-		up_publisher_facade_->publish_debug(response, joint_goals);
+		publisher_facade.publish_throw(joint_goals);
+		publisher_facade.publish_odometry();
+		publisher_facade.publish_debug(response, joint_goals);
 
 		ros::spinOnce();
 		loopRate.sleep();
@@ -102,7 +111,7 @@ void ThrowNode::throw_engine_params_config_callback(bitbots_throw_engine::throw_
 {
 	sp_node_parameter_ = ThrowNodeParameterBuilder::build_from_dynamic_reconf(config, level);
 
-	up_throw_ik_->set_bio_ik_timeout(sp_node_parameter_->bio_ik_time_);
+	ik_.set_bio_ik_timeout(sp_node_parameter_->bio_ik_time_);
 	up_throw_engine_->set_engine_parameter(ThrowEngineParameterBuilder::build_from_dynamic_reconf(config, level));
 }
 
@@ -114,8 +123,6 @@ void ThrowNode::throw_params_config_callback(bitbots_throw_engine::throw_paramsC
 int main(int argc, char **argv)
 {
 	ros::init(argc, argv, "bitbots_throw_engine");
-
 	ThrowNode node;
-
 	ros::spin();
 }
