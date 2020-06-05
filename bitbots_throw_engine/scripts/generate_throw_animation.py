d65645a79d3b2fddb3b23504556340ddcb2a95db
5reichar <5reichar@informatik.uni-hamburg.de>
2020-06-05 15:14:56 +0200

    script works now

--- bitbots_throw_engine/scripts/generate_throw_animation.py
+++ bitbots_throw_engine/scripts/generate_throw_animation.py
@@ -2,10 +2,10 @@
 
 import rospy
 from geometry_msgs.msg import Vector3
-from bitbots_throw import throw_action
+from bitbots_throw.msg import throw_action
 
 rospy.init_node("throw_animation_script")
-throw_publisher = rospy.Publisher("/throw", throw_action)
+throw_publisher = rospy.Publisher("/throw", throw_action, queue_size=1)
 rospy.sleep(1)
 
 throw_ball  = throw_action()
@@ -15,11 +15,11 @@ throw_ball.ball_position.x = 0.3
 throw_ball.ball_position.y = 0.0
 throw_ball.ball_position.z = 0.0
 
-throw_ball.goal_position = Vector3()
-throw_ball.goal_position.x = 2.0
-throw_ball.goal_position.y = 2.0
-throw_ball.goal_position.z = 0.0
+throw_ball.throw_target = Vector3()
+throw_ball.throw_target.x = 2.0
+throw_ball.throw_target.y = 2.0
+throw_ball.throw_target.z = 0.0
 
 throw_publisher.publish(throw_ball)
 rospy.sleep(3)
-rospy.loginfo("Your throw has benn animated")
+rospy.loginfo("Your throw has been animated")
