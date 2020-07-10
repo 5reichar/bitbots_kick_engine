2784b2c87918b651fef0a4713406fd4190bc3cbe
5reichar <5reichar@informatik.uni-hamburg.de>
2020-07-10 12:25:26 +0200

    option for more infos added to the system_publisher.h

--- bitbots_throw_engine/include/ros_interface/publisher/system_publisher.h
+++ bitbots_throw_engine/include/ros_interface/publisher/system_publisher.h
@@ -7,31 +7,43 @@
 namespace bitbots_throw{
     class SystemPublisher{
     public:
-        static void publish_fatal(const std::string & message){
-            ROS_FATAL_STREAM(message);
+        static void publish_fatal(const std::string & message, std::string object_info = ""){
+            ROS_FATAL_STREAM(create_string_for_ros(get_package_id(), object_info, message));
         };
 
-        static void publish_error(const std::string & message){
-            ROS_ERROR_STREAM(message);
+        static void publish_error(const std::string & message, std::string object_info = ""){
+            ROS_ERROR_STREAM(create_string_for_ros(get_package_id(), object_info, message));
         };
 
-        static void publish_warning(const std::string & message){
-            ROS_WARN_STREAM(message);
+        static void publish_warning(const std::string & message, std::string object_info = ""){
+            ROS_WARN_STREAM(create_string_for_ros(get_package_id(), object_info, message));
         };
 
-        static void publish_info(const std::string & message){
-            ROS_INFO_STREAM(message);
+        static void publish_info(const std::string & message, std::string object_info = ""){
+            ROS_INFO_STREAM(create_string_for_ros(get_package_id(), object_info, message));
         };
 
-        static void publish_debug(const std::string & message){
-            ROS_DEBUG_STREAM(message);
+        static void publish_debug(const std::string & message, std::string object_info = ""){
+            ROS_DEBUG_STREAM(create_string_for_ros(get_package_id(), object_info, message));
         };
 
-        static void publish_runtime_error(const std::runtime_error& error){
-            ROS_ERROR_STREAM(error.what());
+        static void publish_runtime_error(const std::runtime_error& error, std::string object_info = ""){
+            ROS_ERROR_STREAM(create_string_for_ros(get_package_id(), object_info, error.what()));
         };
 
     private:
+        static std::string get_package_id(){
+            return "Bitbots Throw";
+        };
+
+        static std::string create_string_for_ros(std::string const & package_id, std::string const & object_info, std::string const & message){
+            std::string ros_message = "[" + package_id;
+            if (object_info != ""){
+                ros_message += " : " + object_info;
+            }
+            ros_message += "]:  " + message;
+            return ros_message;
+        }
     };
 } //bitbots_throw
 #endif //BITBOTS_THROW_SYSTEM_PUBLISHER_H
