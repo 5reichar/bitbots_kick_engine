b920dd29ca35a768b88e6bdfd9e54a5d26c543f0
5reichar <5reichar@informatik.uni-hamburg.de>
2020-09-17 12:22:42 +0200

    calculate_throw_release_point is not static anymore

--- bitbots_throw_engine/include/utility/throw_math.h
+++ bitbots_throw_engine/include/utility/throw_math.h
@@ -23,19 +23,18 @@ namespace bitbots_throw{
             return atan((point.y_ / point.x_));
         }
 
-        static Struct3d calculate_throw_release_point(double const & throw_release_angle
+        Struct3d calculate_throw_release_point(double const & throw_release_angle
                                                       ,double const & arm_length
                                                       ,double const & throw_orientation_angle
                                                       ,double const & throw_zenith){
-            ThrowMath throw_math;
             Struct3d throw_release_point;
             // From Isosceles triangle with the two angles with the same size are given as 90 - throw_angle
-            auto hypotenuse = std::sqrt(2 * std::pow(arm_length, 2) * (1 - throw_math.cos_deg(2 * throw_release_angle)));
-            auto opposite = throw_math.sin_deg(throw_release_angle) * hypotenuse;
-            auto adjacent = throw_math.cos_deg(throw_release_angle) * hypotenuse;
+            auto hypotenuse = std::sqrt(2 * std::pow(arm_length, 2) * (1 - cos_deg(2 * throw_release_angle)));
+            auto opposite = sin_deg(throw_release_angle) * hypotenuse;
+            auto adjacent = cos_deg(throw_release_angle) * hypotenuse;
 
-            throw_release_point.x_ = throw_math.cos_deg(throw_orientation_angle) * adjacent;
-            throw_release_point.y_ = throw_math.sin_deg(throw_orientation_angle) * adjacent;
+            throw_release_point.x_ = cos_deg(throw_orientation_angle) * adjacent;
+            throw_release_point.y_ = sin_deg(throw_orientation_angle) * adjacent;
             throw_release_point.z_ = throw_zenith - opposite;
 
             return throw_release_point;
